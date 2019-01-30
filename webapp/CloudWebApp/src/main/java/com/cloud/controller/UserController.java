23f8a83b6876a9464a4b7b56960d3559dcfdfc55
withsumanth <withsumanth@gmail.com>
2019-01-30 17:46:25 -0500

    Service Dao pojo methods

--- webapp/CloudWebApp/src/main/java/com/cloud/controller/UserController.java
+++ webapp/CloudWebApp/src/main/java/com/cloud/controller/UserController.java
@@ -5,10 +5,16 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,9 +48,10 @@ public class UserController {
 				m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
 				return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
 			}else {
-				Pattern pattern = Pattern.compile(emailPattern);
-		        Matcher emailMatcher = pattern.matcher(user.getUserEmail());
-		        Matcher passwordMatcher = pattern.matcher(user.getPassword());
+				Pattern patternForEmail = Pattern.compile(emailPattern);
+				Pattern patternForPassword = Pattern.compile(passwordPattern);
+		        Matcher emailMatcher = patternForEmail.matcher(user.getUserEmail());
+		        Matcher passwordMatcher = patternForPassword.matcher(user.getPassword());
 		        if(!emailMatcher.matches()) {
 		        	m.put("message", "Email Pattern is wrong");
 		        	m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
@@ -54,11 +61,18 @@ public class UserController {
 		        	m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
 		        	return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
 		        }
-				userService.save(user);
-				m.put("message", "account created successfully");
-				m.put("email", user.getUserEmail());
-				m.put("status",HttpStatus.OK.toString());
-				return new ResponseEntity<Map<String,Object>>(m,HttpStatus.OK);
+		        User userExists = userService.findByUserEmail(user.getUserEmail());
+		        if(userExists == null) {
+		        	userService.save(user);
+					m.put("message", "account created successfully");
+					m.put("email", user.getUserEmail());
+					m.put("status",HttpStatus.OK.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.OK);
+		        }else {
+		        	m.put("message", "Username already exists");
+					m.put("status",HttpStatus.CONFLICT.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.CONFLICT);
+		        }
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
--- webapp/CloudWebApp/src/main/java/com/cloud/service/impl/UserServiceImpl.java
+++ webapp/CloudWebApp/src/main/java/com/cloud/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cloud.service.impl;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,15 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User save(User u) {
-		return userDao.save(u);
+		User user = new User();
+		user.setId(u.getId());
+		user.setUserEmail(u.getUserEmail());
+		user.setPassword(BCrypt.hashpw(u.getPassword(), BCrypt.gensalt()));
+		return userDao.save(user);
+	}
+
+	@Override
+	public User findByUserEmail(String userEmail) {
+		return userDao.findByUserEmail(userEmail);
 	}
 }
