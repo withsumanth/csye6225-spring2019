bda659d3a7b090812e6ee63e18b63cea1c7ce3fa
Prashant <kabra.p@husky.neu.edu>
2019-01-31 00:34:30 -0500

    Added user registration method to controller

--- webapp/CloudWebApp/src/main/java/com/cloud/controller/UserController.java
+++ webapp/CloudWebApp/src/main/java/com/cloud/controller/UserController.java
@@ -28,5 +28,55 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	
+
+	@RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes="application/json")
+	public ResponseEntity<Map<String,Object>> registerUser(@RequestBody User user) {
+		Map<String,Object> m = new HashMap<String,Object>();
+		try {
+			String emailPattern = "^([a-zA-Z0-9_.+-])+\\@(([a-zA-Z0-9-])+\\.)+([a-zA-Z0-9]{2,4})+$";
+			String passwordPattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+			if(user == null) {
+				m.put("message", "Please send username and password");
+				m.put("status",HttpStatus.BAD_REQUEST.toString());
+				return new ResponseEntity<Map<String,Object>>(m,HttpStatus.BAD_REQUEST);
+			}else if(user.getUserEmail() == null) {
+				m.put("message", "Email is null or json format is not correct");
+				m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+				return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
+			}else if(user.getPassword() == null) {
+				m.put("message", "Password is or json format is not correct");
+				m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+				return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
+			}else {
+				Pattern patternForEmail = Pattern.compile(emailPattern);
+				Pattern patternForPassword = Pattern.compile(passwordPattern);
+				Matcher emailMatcher = patternForEmail.matcher(user.getUserEmail());
+				Matcher passwordMatcher = patternForPassword.matcher(user.getPassword());
+				if(!emailMatcher.matches()) {
+					m.put("message", "Email Pattern is wrong");
+					m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
+				}else if(!passwordMatcher.matches()) {
+					m.put("message", "Password Pattern is wrong");
+					m.put("status",HttpStatus.NOT_ACCEPTABLE.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.NOT_ACCEPTABLE);
+				}
+				User userExists = userService.findByUserEmail(user.getUserEmail());
+				if(userExists == null) {
+					userService.save(user);
+					m.put("message", "account created successfully");
+					m.put("email", user.getUserEmail());
+					m.put("status",HttpStatus.OK.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.OK);
+				}else {
+					m.put("message", "Username already exists");
+					m.put("status",HttpStatus.CONFLICT.toString());
+					return new ResponseEntity<Map<String,Object>>(m,HttpStatus.CONFLICT);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
