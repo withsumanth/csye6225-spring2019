447616d074e62407c4b35aa64cf25f519c1f9bde
Sumanth <withsumanth92@gmail.com>
2019-03-13 21:48:44 -0400

    changed instance profile settings

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,29 +1,28 @@
 STACK_NAME=$1
 KEY_NAME=$2
-BucketName=$3
 if [ -z "$1" ]; then
 	echo "Please provide a stack name"
 else
   if [ -z "$2" ]; then
   	echo "Please provide a Aws keyPair name"
   else
-		if [ -z "$3" ]; then
-			echo "Please provide bucket name"
+		echo "Creating application stack"
+		domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+		bname=${domain::-1}
+		BucketName="${bname}.csye6225.com"
+		echo $BucketName
+		AMI_ID=$(aws ec2 describe-images --filters "Name=tag:Base_AMI_Name,Values=ami-9887c6e7" --query 'Images[*].{ID:ImageId}' --output text)
+		stackCreation=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application-stack.json --parameters ParameterKey=stackName,ParameterValue=$STACK_NAME ParameterKey=keyPair,ParameterValue=$KEY_NAME ParameterKey=amiId,ParameterValue=$AMI_ID ParameterKey=s3Bucket,ParameterValue=$BucketName)
+		if [ $? -eq 0 ]; then
+			stackCompletion=$(aws cloudformation wait stack-create-complete --stack-name $STACK_NAME)
+			if [ $? -eq 0 ]; then
+				echo "Stack creation successful"
+			else
+				echo "Error in creating CloudFormation"
+			fi
 		else
-			echo "Creating application stack"
-	    AMI_ID=$(aws ec2 describe-images --filters "Name=tag:Base_AMI_Name,Values=ami-9887c6e7" --query 'Images[*].{ID:ImageId}' --output text)
-	    stackCreation=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application-stack.json --parameters ParameterKey=stackName,ParameterValue=$STACK_NAME ParameterKey=keyPair,ParameterValue=$KEY_NAME ParameterKey=amiId,ParameterValue=$AMI_ID ParameterKey=s3Bucket,ParameterValue=$BucketName)
-	    if [ $? -eq 0 ]; then
-	      stackCompletion=$(aws cloudformation wait stack-create-complete --stack-name $STACK_NAME)
-	      if [ $? -eq 0 ]; then
-	        echo "Stack creation successful"
-	      else
-	        echo "Error in creating CloudFormation"
-	      fi
-	    else
-	      echo "Error in creating CloudFormation"
-	      echo $stackCreation
-	    fi
+			echo "Error in creating CloudFormation"
+			echo $stackCreation
 		fi
   fi
 fi
