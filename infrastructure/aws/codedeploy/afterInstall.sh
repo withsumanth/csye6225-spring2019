181d2eb055387fa5464ebc96df9da841a7b19cda
Prashant <kabra.p@husky.neu.edu>
2019-03-20 21:01:55 -0400

    added before install, application start/stop

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,23 +2,17 @@
 
 sudo systemctl stop tomcat.service
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT /opt/tomcat/webapps/webapp
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
 
-sudo chown tomcat:tomcat /opt/tomcat/webapps/webapp.war
+sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
 # cleanup log files
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
 
-sudo systemctl start tomcat.service
-
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
     -c file:/opt/cloudwatch-config.json \
     -s
-
-sleep 8
-
-sudo systemctl restart tomcat.service
---
+++ infrastructure/aws/codedeploy/beforeinstall.sh
@@ -0,0 +1,1 @@
+echo "Inside before install"
